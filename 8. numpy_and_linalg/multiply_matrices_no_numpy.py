d4c3ee05ca0af86b6274955b99a7bf1e3464f730
Arkadiy Shuvaev <a.shuvaev@ev-digitalinvest.de>
2023-11-19 19:52:42 +0000

    temp commit - calculation

--- 8. numpy_and_linalg/multiply_matrices_no_numpy.py
+++ 8. numpy_and_linalg/multiply_matrices_no_numpy.py
@@ -1,6 +1,4 @@
 import numpy as np
-import random
-import scipy.stats as sps
 
 def no_numpy_mult(a, b):
     """
@@ -13,41 +11,61 @@ def no_numpy_mult(a, b):
 
     Реализуйте умножение матриц без использования функций из пакета numpy
     """
-    result = np.array([a.shape[0], b.shape[1]])
-    print(result)
 
     def ensure_size(a, b):
       if (a.ndim != b.ndim):
         raise Exception("ndim")
 
-      if (a.shape[0] != b.shape[1]):
+      if (a.shape[1] != b.shape[0]):
         raise Exception("shape")
 
-    def calculate_column(a, b, column_b_idx):
-      result = np.empty(a.shape[0])
-      for row_idx_in_a in range(a.shape[0]):
-        for item_idx_in_column_b in range(b.shape[1]):
-          break
+    def calculate_column(a, b, column_idx_of_b):
+      column_values_of_b = b[:, column_idx_of_b]
+      calculation_result = np.zeros(a.shape[0])
+      for row_idx_of_a in range(a.shape[0]):
+        for column_value_idx in range(len(column_values_of_b)):
+          # multiply columns of matrix "a" by the column of matrix "b"
+          calculation_result[row_idx_of_a] += a[row_idx_of_a, column_value_idx] * column_values_of_b[column_value_idx]
 
+      return calculation_result
 
-    # def multiply_matrices(a, b):
-    #   for column_idx_in_b in range(b.shape[1]):
-    #     col = calculate_column(a, b, column_idx_in_b)
-    #     result[column_b_idx] = col
+    def set_result(result_matrix, column_index, column_values):
+      for idx in range(result_matrix.shape[0]):
+        result_matrix[idx, column_index] = column_values[idx]
 
+    def multiply_matrices(a, b):
+      result = np.empty([a.shape[0], b.shape[1]])
 
+      for column_idx_of_b in range(b.shape[1]):
+        col = calculate_column(a, b, column_idx_of_b)
+        set_result(result, column_idx_of_b, col)
+
+      return result
 
     ensure_size(a, b)
-    return a
+    result = multiply_matrices(a, b)
+
+    return result
 
 
-a = np.random.sample((2, 5))
-b = np.random.sample((5, 3))
+def numpy_mult(first, second):
+    """
+    A: np.array[size, size]              --- первая матрица-аргумент
+    B: np.array[size, size]              --- вторая матрица-аргумент
+    return C: np.array[size, size]       --- матрица, являющаяся результатом умножения матриц A и B
 
-print(a)
-print(b)
+    Функция принимает на вход две матрицы: A и B размерностью size x size
+    Возвращает матрицу их произведения A * B = C
 
-print(a.shape[1])
-print(b.shape[0])
+    Реализуйте умножение матриц, используя функции из пакета numpy    """
 
-# no_numpy_mult(a, b)
+    return first @ second
+
+# a = np.random.sample((2, 5))
+# b = np.random.sample((5, 3))
+
+a = np.array([[0, 2, 4, 6, 8], [1, 3, 5, 7, 9]], dtype = float)
+b = np.array([[0, 5, 1], [1, 6, 2], [2, 7, 3], [3, 8, 4], [4, 0, 5]], dtype = float)
+
+print(no_numpy_mult(a, b))
+print(numpy_mult(a, b))
